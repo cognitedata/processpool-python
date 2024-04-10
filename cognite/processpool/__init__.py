7ef348ea50761386866bcfeb86faa80f26a0f8a2
Morten Mjelva <morten.mjelva@cognite.com>
2024-04-10 10:24:27 +0200

    Add deprecation/archive notice

--- cognite/processpool/__init__.py
+++
@@ -1,3 +0,0 @@
-from cognite.processpool.processpool import ProcessPool, ProcessPoolShutDownException, WorkerDiedException  # noqa
-
-__all__ = ["ProcessPool", "ProcessPoolShutDownException", "WorkerDiedException"]
